d8ab9813909b3f2723770ab4902ca7fe3882aeff
David Crammer <dcrammer777@gmail.com>
2023-11-06 17:22:23 -0700

    Change meta info

--- components/UI/MainCard.js
+++ components/UI/MainCard.js
@@ -1,4 +1,5 @@
 import useData from "./useData"
+import Head from "next/head"
 import ProfileSection from "./ProfileSection"
 import { Card, CardBody, Spinner } from "@nextui-org/react"
 import { motion } from "framer-motion"
@@ -43,15 +44,21 @@ export default function MainCard() {
   }
 
   return (
-    <motion.div {...data.cardMotion}>
-      <Card className="w-full h-full sm:w-[550px] sm:h-[650px] p-7 max-w-full flex rounded-2xl shadow-2xl bg-offWhite">
-        <CardBody className="flex flex-col gap-6 justify-between">
-          <ProfileSection {...data} />
-          <div ref={data.scope} className="stretch flex-grow flex flex-col items-center justify-between gap-4">
-            <data.CurrentComponent {...data} />
-          </div>
-        </CardBody>
-      </Card>
-    </motion.div>
+    <>
+      <Head>
+        <title>{data.userName} Wants Reviews</title>
+        <link rel="icon" href={data.userImage || "/images/star-solid.png"} />
+      </Head>
+      <motion.div {...data.cardMotion}>
+        <Card className="w-full min-h-full sm:w-[550px] sm:min-h-[650px] p-7 max-w-full flex rounded-2xl shadow-2xl bg-offWhite">
+          <CardBody className="flex flex-col gap-6 justify-between">
+            <ProfileSection {...data} />
+            <div ref={data.scope} className="stretch flex-grow flex flex-col items-center justify-between gap-4">
+              <data.CurrentComponent {...data} />
+            </div>
+          </CardBody>
+        </Card>
+      </motion.div>
+    </>
   )
 }
